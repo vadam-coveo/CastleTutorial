66a6421feb570266a1d1f9ab5c464bc5dfa75ed2
Marilyne Chan <mchan@coveo.com>
2023-05-02 15:29:52 -0400

    visa

--- CastleTurorial/Start/Demos/Challenge/Challenge1/Challenge1Installer.cs
+++ CastleTurorial/Start/Demos/Challenge/Challenge1/Challenge1Installer.cs
@@ -11,13 +11,19 @@ namespace Start.Demos.Challenge.Challenge1
         public void Install(IWindsorContainer container, IConfigurationStore store)
         {
             container.Kernel.Resolver.AddSubResolver(new ListResolver(container.Kernel, true));
+
+            IDatabaseConfiguration visaConfiguration = new DatabaseConfiguration("Visa");
             
             container.Register(
                 Component.For<ICanBeDemoed>().ImplementedBy<Challenge1Demo>(),
                 Component.For<IDatabaseConfiguration>()
                     .ImplementedBy<DatabaseConfiguration>()
                     .DependsOn(Dependency.OnValue<string>("Mastercard"))
-                );
+                    .Named("Mastercard"),
+                Component.For<IDatabaseConfiguration>()
+                    .Instance(visaConfiguration)
+                    .Named("visa")
+            );
         }
     }
 }
